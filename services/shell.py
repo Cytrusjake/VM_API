d90a5a2c75970b87627eedb5c86f15729705e4b1
Jacob Billings <jacobbillings@Jacobs-Air.home>
2026-08-06 22:02:55 +0200

    fixed shell service

--- services/shell.py
+++ services/shell.py
@@ -1,29 +1,62 @@
-# services/shell.py
-
 import subprocess
+
+from result import Result
 
 
 class ShellService:
 
+    ####################################################################
+    # Execute
+    ####################################################################
+
     def execute(
         self,
         command,
-        cwd     = None,
-        timeout = None
+        cwd=None,
+        timeout=None
     ):
 
         result = subprocess.run(
             command,
-            shell           = True,
-            cwd             = cwd,
-            timeout         = timeout,
-            capture_output  = True,
-            text            = True
+            shell=True,
+            cwd=cwd,
+            timeout=timeout,
+            capture_output=True,
+            text=True
         )
 
         return {
-            "success"       : result.returncode == 0,
-            "returncode"    : result.returncode,
-            "stdout"        : result.stdout.strip(),
-            "stderr"        : result.stderr.strip()
+            "success": result.returncode == 0,
+            "returncode": result.returncode,
+            "stdout": result.stdout.strip(),
+            "stderr": result.stderr.strip()
         }
+
+    ####################################################################
+    # Execute and return Result
+    ####################################################################
+
+    def run_result(
+        self,
+        command,
+        cwd=None,
+        timeout=None
+    ):
+
+        response = self.execute(
+            command,
+            cwd=cwd,
+            timeout=timeout
+        )
+
+        if response["success"]:
+
+            return Result.success_result(
+                data={
+                    "stdout": response["stdout"]
+                }
+            )
+
+        return Result.failed_result(
+            error=response["stderr"]
+        )
